5ebda34c3e323e916b41d20db4ff103639926c59
Preetam Kumar <2000kpritam@gmail.com>
2025-04-02 00:04:51 +0530

    Fetch News from API(27)

--- newsmonkey/src/components/NewsItem.js
+++ newsmonkey/src/components/NewsItem.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title, description}= this.props;
+    let {title, description, imageUrl, newsUrl}= this.props;
     return (
-      <div>
+      <div className="my-3">
         <div className="card" style={{width: "18rem"}}>
-        <img src="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg" className="card-img-top" alt="..."/>
+        <img src={imageUrl?imageUrl:"https://cdn.mos.cms.futurecdn.net/7f4LnhCWK2XCRqEc9dPo3V-1200-80.jpg"} className="card-img-top" alt="..."/>
         <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/newsitem" className="btn btn-primary">Go somewhere</a>
+            <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
         </div>
         </div>
       </div>
