8d413071447c99c5089b678ec1ca6867cc0ea8ed
Preetam Kumar <2000kpritam@gmail.com>
2025-04-28 22:04:22 +0530

    Add Prev and Next button

--- newsmonkey/src/components/NewsItem.js
+++ newsmonkey/src/components/NewsItem.js
@@ -8,9 +8,9 @@ export class NewsItem extends Component {
         <div className="card" style={{width: "18rem"}}>
         <img src={imageUrl?imageUrl:"https://cdn.mos.cms.futurecdn.net/7f4LnhCWK2XCRqEc9dPo3V-1200-80.jpg"} className="card-img-top" alt="..."/>
         <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
         </div>
         </div>
       </div>
